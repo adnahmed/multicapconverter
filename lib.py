1e1d4bd5c1baaef0d884c43d7be66d0da190210c
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-10-10 14:19:56 +0500

    add unhexlify to lib

---
+++ lib.py
@@ -0,0 +1,32 @@
+table_hex = [
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    0, 1, 2, 3,  4, 5, 6, 7,  8, 9,-1,-1, -1,-1,-1,-1,
+    -1,10,11,12, 13,14,15,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,10,11,12, 13,14,15,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1
+]
+
+
+def a2b_hex(t):
+    result = []
+
+    def pairs_gen(s):
+        while s:
+            try:
+                yield table_hex[ord(s[0])], table_hex[ord(s[1])]
+            except IndexError:
+                if len(s):
+                    raise TypeError('Odd-length string')
+                return
+            s = s[2:]
+
+    for a, b in pairs_gen(t):
+        if a < 0 or b < 0:
+            raise TypeError('Non-hexadecimal digit found')
+        result.append(chr((a << 4) + b))
+    return bytes(''.join(result))
+    
+unhexlify = a2b_hex
